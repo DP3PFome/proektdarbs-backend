96e8f74737e883e7fe0fea9673ef55178e86e7c0
DP3PFome <A230676PF@rvt.lv>
2026-05-19 23:33:20 +0300

    fix password cast

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
-
+/*
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/check-db', function () {
+    try {
+        DB::connection()->getPdo();
+        $tables = DB::select("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'");
+        return [
+            'connected' => true,
+            'tables' => $tables
+        ];
+    } catch (\Exception $e) {
+        return [
+            'connected' => false,
+            'error' => $e->getMessage()
+        ];
+    }
 });
